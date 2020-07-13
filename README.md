8d2fb0b8a6aed044709064172ea3d872aab6b60d
Joshua Hawthorne-Madell <jhawthorne13@gmail.com>
2020-07-13 23:50:30 +0200

    Tidied up code, gitignore, and addeda proper README and a state-to-zip reference CSV

---
+++ README.md
@@ -0,0 +1,104 @@
+# Introduction
+
+I created the code to help with a number of marketing inititatives
+that required getting the contact details for various types of
+businesses within a certain area.
+
+The python file *grab\_contacts\_from\_gmaps.py* can be used via a CLI
+or a Python interpretor to generate a CSV file containing the contact
+infromation for all establishments from Google Places for a given US
+state or zipcode.
+
+To use it yourself, you will need to create your own Google API key,
+access to a CLI, and Python 3.
+
+The Google API key is free and will give you a fair number of free
+calls per day, such that you should be able to grab many contacts from
+a given state (or all from a given zipode) for various types of
+establishments.
+
+In this repo, I have also included a state--zipcode CSV that will
+allow you to simply specify a state, and then the code will run
+through all the zipcodes for that state. (Thanks to
+[aggdata](https://www.aggdata.com/) for the free data!)
+
+## Getting Setup
+
+First, clone this repository.
+
+Second, in whatever environment you want to run the script, you can
+install any missing packages by running
+
+`pip install -r requirements.txt`
+
+Third, get a Google API key.  If you don't have one, go
+[here](https://developers.google.com/places/web-service/get-api-key#get_an_api_key)
+to make your own API key, certify it, and then enable "Google Places
+API Web Services" and "Google Maps Geocoding API".
+
+
+## Using it
+
+### From CLI
+
+First, change your working directory to the directory where
+both this script and zipcode database are. Then, type
+
+`python grab_contact_info.py`
+
+and then simply follow the prompts.
+
+### From Python Interpretor
+
+Load script into your interpretor, and then use one of the following methods:
+
+* *grab\_data\_for\_zip(establishment, zipcode, state_code)*
+* *grab\_data\_for\_state(establishment, state_code)*
+* *grab\_all\_data(establishment, state_list)*
+
+The method should accept the establishment types listed in the [Google
+Places API
+documentation](https://developers.google.com/places/web-service/supported_types),
+and the general state abbreviations for US states.  If you input
+something wrong, the methods will print out the list of accepted inputs.
+
+
+### General Run Notes
+
+When querying via zipcode, the script uses Google's Geocode API to get the
+proper Google Places viewwindow coordinates.
+
+When querying via state, the script looks through the provide
+state--zipcode CSV to enumerate through zipcodes of the given state.
+
+If an error occurs when attempting to access the Places or Geocoding
+API, the status code and URL for the Google API will be printed, and
+an ApiStatusError will be thrown.
+
+
+## Data files and logging behavior
+
+Data and log files will be created in the same directory as this repository.
+
+The "logfile" will keep track of the zipcodes that have been searched
+and do not contain any of the specified establishment.
+
+File structure works as follows
+
+```
+repository/
+|
+|---+[establishment]/
+|   |
+|   |---+[state_code]/
+|       |
+|       |---[zip_code].csv
+|       |---[state_code]_all_zipcodes.csv
+|
+|
+|---+logs/
+    |
+    |---+[state_code]/
+        |
+	|---logfile
+```
--- grab_contacts_from_gmaps.py
+++ grab_contacts_from_gmaps.py
@@ -1,30 +1,12 @@
-'''This contains a script to generate a csv file containing the
+'''This contains a script to generate a CSV file containing the
 contact infromation for all establishments from Google Places for a
 given US state or zipcode.
 
-To use, first, go to
-https://developers.google.com/places/web-service/get-api-key#get_an_api_key
-to make your own API key, certify it, and enable "Google Places API
-Web Services" and "Google Maps Geocoding API".
-
-Second, in a Python interpreter, use the set_key() method, using your
-API key as a string as the input.
-
-Third, from a terminal, make your CWD to a directory where both this
-script and zipcode database are. Then, type "python
-grab_contact_info.py" and follow the prompts.
-
-Alternatively, after your API key is set up you can run the
-grab_all_data() method in a Python interpreter for a given
-establishment and a list of as many states as you'd like.
-
-If an error occurs when attempting to access the Places or Geocoding
-API, your computer will beep, the status code and URL for the Google
-API will be printed, and an ApiStatusError will be thrown.
-
+Please see the README for more information.
 '''
 
 from collections import namedtuple
+from getpass import getpass
 import os
 import pickle
 import re
@@ -56,6 +38,9 @@ class LogFolderError(Exception):
 class ContinueAnyway(Exception):
     pass
 
+class UnacceptedInput(Exception):
+    pass
+
 
 # NAMED TUPLES
 
@@ -70,21 +55,12 @@ AddressComponents = namedtuple('AddressComponents',
 
 # METHODS
 
-def beep(length, frequency=440):
-
-    '''Makes a beeping noise
-    '''
-
-    os.system('play --no-show-progress --null --channels 1 '
-              'synth {} sine {}'.format(length, frequency))
-
-
 def set_key(key, file_name='./.ga_key'):
 
     '''Create a hidden file of google api key from a string
     '''
 
-    with open(file_name, 'w+') as pw:
+    with open(file_name, 'bw+') as pw:
         pickle.dump(key, pw)
 
 
@@ -93,10 +69,90 @@ def get_key(file_name='./.ga_key'):
     '''Returns the google api key needed for all requests
     '''
 
-    with open(file_name, 'r') as pr:
-        key = pickle.load(pr)
+    try:
+        with open(file_name, 'br') as pr:
+            key = pickle.load(pr)
+
+    except FileNotFoundError:
+        answer = input('Google API key not found. Do you want to enter it now? '
+                       '[(y)es/(n)o) :').lower()
+        if answer.starts_with('y'):
+            key = getpass(prompt='Type / Paste API key and then press enter/return: ')
+            set_key(key)
+            print('Saved to filepath default, "./.ga_key"')
+            
+            return key
+
+        else:
+            print('Cannot run without a Google API key. '
+                  'Please try again when you have one.')
+            pass
 
     return key
+
+def accepted_state_code(state_code):
+
+    '''Checks if the inputted state_code is in the provided state--csv zipcode
+    '''
+
+    accepted_state_codes = ['AA', 'AK', 'AL', 'AP', 'AR', 'AZ', 'CA', 'CO', 'CT',
+                            'DC', 'DE', 'FL', 'FM', 'GA', 'HI', 'IA', 'ID', 'IL',
+                            'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MH', 'MI',
+                            'MN', 'MO', 'MP', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH',
+                            'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'PW',
+                            'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA',
+                            'WI', 'WV', 'WY']
+
+    if state_code not in accepted_state_codes:
+        print('Invalid state code. Please use a type from '
+              'the fllowing list\n')
+        print('{}\n'.format(accepted_state_codes))
+
+        raise UnacceptedInput
+
+    return True
+
+
+def accepted_establishment(establishment):
+
+    '''Checks if the inputted establishment is on the Google Places API list
+    '''
+    
+    accepted_types = ['accounting', 'airport', 'amusement_park', 'aquarium',
+                      'art_gallery', 'atm', 'bakery', 'bank', 'bar',
+                      'beauty_salon', 'bicycle_store', 'book_store',
+                      'bowling_alley', 'bus_station', 'cafe', 'campground',
+                      'car_dealer', 'car_rental', 'car_repair', 'car_wash',
+                      'casino', 'cemetery', 'church', 'city_hall',
+                      'clothing_store', 'convenience_store', 'courthouse',
+                      'dentist', 'department_store', 'doctor', 'electrician',
+                      'electronics_store', 'embassy', 'fire_station',
+                      'florist', 'funeral_home', 'furniture_store',
+                      'gas_station', 'gym', 'hair_care', 'hardware_store',
+                      'hindu_temple', 'home_goods_store', 'hospital',
+                      'insurance_agency', 'jewelry_store', 'laundry',
+                      'lawyer', 'library', 'light_rail_station',
+                      'liquor_store', 'local_government_office',
+                      'locksmith', 'lodging', 'meal_delivery',
+                      'meal_takeaway', 'mosque', 'movie_rental',
+                      'movie_theater', 'moving_company', 'museum',
+                      'night_club', 'painter', 'park', 'parking', 'pet_store',
+                      'pharmacy', 'physiotherapist', 'plumber', 'police',
+                      'post_office', 'real_estate_agency', 'restaurant',
+                      'roofing_contractor', 'rv_park', 'school', 'shoe_store',
+                      'shopping_mall', 'spa', 'stadium', 'storage', 'store',
+                      'subway_station', 'synagogue', 'taxi_stand',
+                      'train_station', 'transit_station', 'travel_agency',
+                      'university', 'veterinary_care', 'zoo']
+
+    if establishment not in accepted_types:
+        print('Invalid establishmet type. Please use a type from '
+              'the following list\n')
+        print('{}\n'.format(accepted_types))
+
+        raise UnacceptedInput
+
+    return True
 
 
 def get_string(soup_data):
@@ -116,9 +172,8 @@ def check_status(soup, url):
     '''
 
     if get_string(soup.status) not in ('OK', 'ZERO_RESULTS'):
-        beep(1)
-        print get_string(soup.status)
-        print url
+        print(get_string(soup.status))
+        print(url)
         raise ApiStatusError
 
 
@@ -126,15 +181,9 @@ def make_soup(base_query, payload, rtrn_url=False):
 
     '''Returns a parsable BeautifulSoup object from a HTTP request query
     '''
-
-    # session = requests.Session()
-    # session.mount('https://', HTTPAdapter(max_retries=1))
-    # try:
-    #     page = session.get(base_query, params=payload, verify=False)
     try:
         page = requests.get(base_query, params=payload)
     except (requests.exceptions.SSLError, requests.exceptions.ConnectionError):
-        beep(1)
         raise
     soup = BeautifulSoup(page.text, 'lxml')
     check_status(soup, page.url)
@@ -178,13 +227,9 @@ def get_coordinates(zipcode, err_count=0):
                             lng=soup.geometry.location.lng.string,
                             rad=radius)
     else:
-        beep(1)
-        print 'ZIPCODE {} NOT FOUND IN GOOGLE GEOCODING API'.format(zipcode)
-        print url
+        print('ZIPCODE {} NOT FOUND IN GOOGLE GEOCODING API'.format(zipcode))
+        print(url)
         coors = None
-        # coors = Coordinates(lat=42.42,
-        #                     lng=42.42,
-        #                     rad=1)
 
     return coors
 
@@ -283,7 +328,9 @@ def write_to_log(establishment, zipcode, state_code):
     '''Writes zipcode to logfile when no establishments are found in zipcode
     '''
 
-    logfile = os.path.join(establishment, 'logs', state_code, 'logfile')
+    if not os.path.isdir(os.path.join('data', establishment, 'logs', state_code)):
+            os.makedirs(os.path.join('data', establishment, 'logs', state_code))
+    logfile = os.path.join('data', establishment, 'logs', state_code, 'logfile')
     with open(logfile, 'a+') as wa:
         wa.write('{}\n'.format(zipcode))
 
@@ -302,9 +349,9 @@ def write_establishment_data(data, establishment, zipcode, state_code):
 
     df = df[df.zipcode.str.startswith(zipcode)]
     if not df.empty:
-        if not os.path.isdir(os.path.join(establishment, state_code)):
-            os.makedirs(os.path.join(establishment, state_code))
-        csv_file = os.path.join(establishment, state_code, zipcode) + '.csv'
+        if not os.path.isdir(os.path.join('data', establishment, state_code)):
+            os.makedirs(os.path.join('data', establishment, state_code))
+        csv_file = os.path.join('data', establishment, state_code, zipcode) + '.csv'
         df.to_csv(csv_file, index=False, encoding='utf-8')
         return True
     else:
@@ -318,7 +365,7 @@ def check_current_data(zip_list, establishment, state_code):
     '''
 
     zip_num_all = len(zip_list)
-    data_folder = os.path.join(establishment, state_code)
+    data_folder = os.path.join('data', establishment, state_code)
     if not os.path.isdir(data_folder):
         os.makedirs(data_folder)
 
@@ -337,23 +384,23 @@ def check_current_data(zip_list, establishment, state_code):
         zip_list.remove(z)
     zip_num_wo_created = len(zip_list)
     zip_num_diff_1 = zip_num_all - zip_num_wo_created
-    print '{} zipcode csvs already created for {}'.format(zip_num_diff_1,
-                                                          state_code)
+    print('{} zipcode csvs already created for {}'.format(zip_num_diff_1,
+                                                          state_code))
 
     # Remove already tried zipcodes from zip_list
-    logfolder = os.path.join(establishment, 'logs', state_code)
+    logfolder = os.path.join('data', establishment, 'logs', state_code)
     logfile = os.path.join(logfolder, 'logfile')
     if not os.path.isdir(logfolder):
         os.makedirs(logfolder)
-        open(logfile, 'a').close()
+        open(logfile, 'a+').close()
     with open(logfile, 'r') as r:
         tried_zip_list = [int(l.rstrip('\n')) for l in r.readlines()]
     for z in tried_zip_list:
         zip_list.remove(z)
     zip_num_wo_tried = len(zip_list)
     zip_num_diff_2 = zip_num_wo_created - zip_num_wo_tried
-    print ('{} previously searched zipcodes have no '
-           '{} in {}'.format(zip_num_diff_2, establishment, state_code))
+    print('{} previously searched zipcodes have no '
+          '{} in {}'.format(zip_num_diff_2, establishment, state_code))
 
     return (zip_list, zip_num_diff_1 + zip_num_diff_2)
 
@@ -365,7 +412,10 @@ def grab_data_for_zip(establishment, zipcode, state_code):
     '''Create a csv file containing contact data for a given place-type and zipcode
     '''
 
-    zipcode = '{:05}'.format(zipcode)
+    accepted_establishment(establishment)
+    accepted_state_code(state_code)
+
+    zipcode = '{:05}'.format(int(zipcode))
     coors = get_coordinates(zipcode)
     if coors:
         place_ids = get_place_ids(establishment,
@@ -375,52 +425,58 @@ def grab_data_for_zip(establishment, zipcode, state_code):
         created = write_establishment_data(establisment_data, establishment,
                                            zipcode, state_code)
         if created:
-            print '{} csv for zipcode {} created  '.format(establishment,
-                                                           zipcode),
+            print('{} csv for zipcode {} created  '.format(establishment, zipcode),
+                  end='')
         else:
-            print 'no {} found for zipcode {}     '.format(establishment,
-                                                           zipcode),
+            print('no {} found for zipcode {}     '.format(establishment, zipcode),
+                  end='')
     else:
-        print 'no {} found for zipcode {}     '.format(establishment, zipcode),
+        print('no {} found for zipcode {}     '.format(establishment, zipcode),
+              end='')
 
     return
 
 
 def grab_data_for_state(establishment, state_code):
 
-    '''Create a csv file containing contact data for a given place-type and state
+    '''Create a CSV file containing contact data for a given place-type and state
     '''
 
-    df = pd.read_csv('./zip_code_database.csv', engine='python')
-    zip_list = list(set(df[df.state == state_code]['zip']))
+    state_zip_file = './us_postal_codes.csv'
+    df = pd.read_csv(state_zip_file, engine='python')
+    zip_list = list(set(df[df['State Abbreviation'] == state_code]['Zip Code']))
     zip_num_all = len(zip_list)
     zip_list, zip_num_diff = check_current_data(zip_list, establishment,
                                                 state_code)
 
     for e, z in enumerate(zip_list):
         grab_data_for_zip(establishment, z, state_code)
-        print '|{}/{}|'.format(e + zip_num_diff + 1, zip_num_all)
+        print('|{}/{}|'.format(e + zip_num_diff + 1, zip_num_all))
 
     concatenation_call = ''.join(("awk 'FNR==1 && NR!=1{next;}{print}' ",
-                                  "{}/{}/*.csv > ".format(establishment,
-                                                          state_code),
-                                  "{}/{}/{}".format(establishment,
-                                                    state_code,
-                                                    state_code),
+                                  "data/{}/{}/*.csv > ".format(establishment,
+                                                               state_code),
+                                  "data/{}/{}/{}".format(establishment,
+                                                         state_code,
+                                                         state_code),
                                  "_all_zipcodes.csv"))
     os.system(concatenation_call)
 
-    print '{} csv for {} created'.format(establishment, state_code)
+    print('{} csv for {} created'.format(establishment, state_code))
     return
 
 
-def grab_all_data(establishment, state_list=STATES):
+def grab_all_data(establishment, state_list):
+
+    '''For each state in the state_list, run method grab_data_for_state()
+    '''
+    
     for state in state_list:
         try:
             grab_data_for_state(establishment, state)
         except DataFileExistsError:
-            print 'All {} data from {} already acquired'.format(establishment,
-                                                                state)
+            print('All {} data from {} already acquired'.format(establishment,
+                                                                state))
 
 
 if __name__ == '__main__':
@@ -432,70 +488,26 @@ if __name__ == '__main__':
           'writen, to file:\n'
           './[establishment]/[state]/[state]_all_zipcodes.csv\n\n')
 
-    accepted_types = ['accounting', 'airport', 'amusement_park', 'aquarium',
-                      'art_gallery', 'atm', 'bakery', 'bank', 'bar',
-                      'beauty_salon', 'bicycle_store', 'book_store',
-                      'bowling_alley', 'bus_station', 'cafe', 'campground',
-                      'car_dealer', 'car_rental', 'car_repair', 'car_wash',
-                      'casino', 'cemetery', 'church', 'city_hall',
-                      'clothing_store', 'convenience_store', 'courthouse',
-                      'dentist', 'department_store', 'doctor', 'electrician',
-                      'electronics_store', 'embassy', 'fire_station',
-                      'florist', 'funeral_home', 'furniture_store',
-                      'gas_station', 'grocery_or_supermarket', 'gym',
-                      'hair_care', 'hardware_store', 'hindu_temple',
-                      'home_goods_store', 'hospital', 'insurance_agency',
-                      'jewelry_store', 'laundry', 'lawyer', 'library',
-                      'liquor_store', 'local_government_office',
-                      'locksmith', 'lodging', 'meal_delivery',
-                      'meal_takeaway', 'mosque', 'movie_rental',
-                      'movie_theater', 'moving_company', 'museum',
-                      'night_club', 'painter', 'park', 'parking', 'pet_store',
-                      'pharmacy', 'physiotherapist', 'plumber', 'police',
-                      'post_office', 'real_estate_agency', 'restaurant',
-                      'roofing_contractor', 'rv_park', 'school', 'shoe_store',
-                      'shopping_mall', 'spa', 'stadium', 'storage', 'store',
-                      'subway_station', 'synagogue', 'taxi_stand',
-                      'train_station', 'transit_station', 'travel_agency',
-                      'university', 'veterinary_care', 'zoo']
+    establishment = input('Establishment type?: ').lower()
+    accepted_establishment(establishment)
 
-    accepted_states = ['WA', 'VA', 'DE', 'DC', 'WI', 'WV', 'HI', 'CO',
-                       'FL', 'FM', 'WY', 'PR', 'NJ', 'NM', 'TX', 'LA',
-                       'AK', 'NC', 'ND', 'NE', 'TN', 'NY', 'PA', 'RI',
-                       'NV', 'AA', 'NH', 'GU', 'AE', 'PW', 'VI', 'CA',
-                       'AL', 'AP', 'AS', 'AR', 'VT', 'IL', 'GA', 'IN',
-                       'IA', 'OK', 'AZ', 'ID', 'CT', 'ME', 'MD', 'MA',
-                       'OH', 'UT', 'MO', 'MN', 'MI', 'MH', 'KS', 'MT',
-                       'MP', 'MS', 'SC', 'KY', 'OR', 'SD']
+    state_code = input('Two-letter state code: ').upper()
+    accepted_state_code(state_code)
 
-    establishment = raw_input('Establishment type?: ').lower()
-    if establishment not in accepted_types:
-        print('Invalid establishmet type. Please use a type from '
-              'the following list\n')
-        print accepted_types
-        print
-        sys.exit(1)
-
-    state_code = raw_input('Two-letter state code: ').upper()
-    if state_code not in accepted_states:
-        print 'Invalid state code\n'
-        sys.exit(1)
-
-    answer = raw_input('Search the entire state, or a specific zipcode? '
+    answer = input('Search the entire state, or a specific zipcode? '
                        '[(z)ip/(s)tate]: ').lower()
     if answer.startswith('z'):
-        zipcode = raw_input('5-digit zipcode: ')
+        zipcode = input('5-digit zipcode: ')
         if not re.match('\d\d\d\d\d', zipcode):
-            print 'Invalid zipcode\n'
+            print('Invalid zipcode\n')
             sys.exit(1)
-        print 'Working...'
+        print('Working...')
         grab_data_for_zip(establishment, zipcode, state_code)
         sys.exit(0)
     elif answer.startswith('s'):
-        print 'Working...'
+        print('Working...')
         grab_data_for_state(establishment, state_code)
         sys.exit(0)
-
     else:
-        print 'Invalid answer\n'
+        print('Invalid answer\n')
         sys.exit(1)
